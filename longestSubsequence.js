90df8cdaf871740a01c0d3a854c63c92de20238d
Tyrone Saulon <tyrone.saulon@hotmail.com>
2018-12-16 02:08:27 -0500

    Optimizing

--- longestSubsequence.js
+++ longestSubsequence.js
@@ -3,32 +3,33 @@ let words = ["able", "ale", "apple", "bale", "kangaroo", "peel", "leap"];
 
 //  find longest subsequence inside D using sequence S
 function foo(S, D) {
+  
+  try {
+    //  sort descending and iterate through each word inside D 
+    D.sort((a, b) => b.length - a.length).forEach(word => {
 
-  let longestSubsequence = '';
+      //  flag subtractions
+      let seq = S.split('');
+      let tmpWord = word.split('');
 
-  //  iterate through each word inside D
-  D.forEach(word => {
+      //  iterate through each letter inside word
+      word.split('').forEach((letter, index) => {
+        if (seq.includes(letter)) {
+          seq.splice(0, seq.indexOf(letter)); //  remove letters 0 up to index
+          tmpWord.splice(tmpWord.indexOf(letter), 1); //  remove letter from tmpWord
+        }
+      })
 
-    //  flag subtractions
-    let seq = S.split('');
-    let tmpWord = word.split('');
-
-    //  iterate through each letter inside word
-    word.split('').forEach((letter, index, array) => {
-      if (seq.includes(letter)) {
-        seq.splice(0, seq.indexOf(letter)); //  remove letters 0 up to index
-        tmpWord.splice(tmpWord.indexOf(letter), 1); //  remove letter from tmpWord
+      //  throw longestSequence
+      if (tmpWord.length === 0) {
+        throw word;
       }
+    });
+  } catch (word) {
+    return word;
+  }
 
-      //  set longestSubsequence if all letters from tmpWord can be subtracted from seq
-      //  and the subsequence is longer than longestSubsequence
-      if (tmpWord.length === 0 && word.length > longestSubsequence.length) {
-        longestSubsequence = word;
-      }
-    })
-  })
-
-  return longestSubsequence;
+  return null;
 }
 
 console.log(foo(str, words));
